e3b37a4739f60e2c61fbb98b84c55d7e46bfc36f
harsh gajera <harshgajera202@gmail.com>
2024-01-20 09:35:59 +0530

    commit

--- controllers/PortfolioWebsite.js
+++ controllers/PortfolioWebsite.js
@@ -59,30 +59,21 @@ const InsertPortfolioForm = async (req, res, next) =>{
     };
 
     const emailHtml = ejs.render(emailTemplate, data)
-    
-    // Create a Transporter object
+  
+    // Send mail
     const Transporter = nodemailer.createTransport({
       service: 'gmail',
+      host: "smtp.gmail.net",
+      port: 465,
+      secure: true,
       auth: {
-          type: 'OAuth2',
-          user: process.env.MAIL_USERNAME,
-          clientId: process.env.OAUTH_CLIENTID,
-          clientSecret: process.env.OAUTH_CLIENT_SECRET,
-          refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-          accesstoken: process.env.OAUTH_ACCESS_TOKEN
-      }
+        user: process.env.RECEIVE_MAIL,
+        pass: process.env.APP_PASSWORD,
+      },
     })
-    // const Transporter = nodemailer.createTransport({
-    //   host: 'localhost',
-    //   port: 1025,
-    //   auth: {
-    //       user: 'project.1',
-    //       pass: 'secret.1'
-    //   }
-    // })
 
     let mailOptions = {
-      form : req.body.email,
+      from : req.body.email,
       to : process.env.RECEIVE_MAIL,
       subject: 'Contact Mail',
       html: emailHtml,
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from 'dotenv'
+import helmet from "helmet";
 import './Config/DB.js'
 import router from "./route.js"
 
@@ -10,6 +11,7 @@ const app = express()
 const Port = process.env.PORT || 8080
 
 app.use(express.json())
+app.use(helmet())
 app.use(cors())
 
 app.use('/',router)
